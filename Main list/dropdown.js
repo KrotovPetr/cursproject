7c4cdae2ab247ce55b837de637a69b0449dbdcdf
KrotovPetr <83280920+KrotovPetr@users.noreply.github.com>
2021-11-12 22:52:06 +0300

    99% done

--- Main list/dropdown.js
+++ Main list/dropdown.js
@@ -1,5 +1,5 @@
-function myFunction(e){
-    let isActive = document.getElementById("submenu").getAttribute("active")
+function myFunction(e,id){
+    let isActive = document.getElementById("submenu").getAttribute("active");
     if(isActive?.toString()==="true"){
         document.getElementById("submenu").setAttribute("active","false")
     }else{
@@ -7,41 +7,14 @@ function myFunction(e){
     }
 }
 
-
-
-function myFunction2(e){
-    let isActive = document.getElementById("name").getAttribute("active")
+function myFunction3(e,id){
+    let isActive = document.getElementById(id).getAttribute("active");
     if(isActive?.toString()==="true"){
-        document.getElementById("name").setAttribute("active","false")
+        document.getElementById(id).setAttribute("active","false")
     }else{
-        document.getElementById("name").setAttribute("active","true")
+        document.getElementById(id).setAttribute("active","true")
     }
 }
 
 
-function myFunction3(e){
-    let isActive = document.getElementsByClassName("submenu2").getAttribute("active")
-    let isActive2 = document.getElementsByClassName("submenu3").getAttribute("active")
-    let isActive3 = document.getElementsByClassName("submenu4").getAttribute("active")
-    let isActive4 = document.getElementsByClassName("submenu5").getAttribute("active")
-    if(isActive?.toString()==="true"){
-        document.getElementsByClassName('submenu2').setAttribute("active","false")
-    }else{
-        document.getElementsByClassName("submenu2").setAttribute("active","true")
-    }
-    if(isActive2?.toString()==="true"){
-        document.getElementsByClassName('submenu3').setAttribute("active","false")
-    }else{
-        document.getElementsByClassName("submenu3").setAttribute("active","true")
-    }
-    if(isActive3?.toString()==="true"){
-        document.getElementsByClassName('submenu4').setAttribute("active","false")
-    }else{
-        document.getElementsByClassName("submenu4").setAttribute("active","true")
-    }
-    if(isActive4?.toString()==="true"){
-        document.getElementsByClassName('submenu5').setAttribute("active","false")
-    }else{
-        document.getElementsByClassName("submenu5").setAttribute("active","true")
-    }
-}
+
