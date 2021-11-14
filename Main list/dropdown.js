7128bf53374f8ba056225b49621b998764ef537b
KrotovPetr <83280920+KrotovPetr@users.noreply.github.com>
2021-11-14 12:54:24 +0300

    Add people card

--- Main list/dropdown.js
+++ Main list/dropdown.js
@@ -17,4 +17,94 @@ function myFunction3(e,id){
 }
 
 
+function myFunction4(у){
+    let cards=document.getElementsByClassName("newsCard");
+
+    cards[0].addEventListener("mouseover", function(event){
+        cards[1].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+        cards[5].style.filter="blur(5px)";
+    },false);
+    cards[0].addEventListener("mouseout", function(event){
+        cards[1].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+        cards[5].style.filter="blur(0px)";
+    },false);
+
+    cards[1].addEventListener("mouseover", function(event){
+        cards[0].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+        cards[5].style.filter="blur(5px)";
+    },false)
+    cards[1].addEventListener("mouseout", function(event){
+        cards[0].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+        cards[5].style.filter="blur(0px)";
+    },false)
+    cards[2].addEventListener("mouseover", function(event){
+        cards[1].style.filter="blur(5px)";
+        cards[0].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+        cards[5].style.filter="blur(5px)";
+    },false)
+    cards[2].addEventListener("mouseout", function(event){
+        cards[1].style.filter="blur(0px)";
+        cards[0].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+        cards[5].style.filter="blur(0px)";
+    },false)
+    cards[3].addEventListener("mouseover", function(event){
+        cards[1].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[0].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+        cards[5].style.filter="blur(5px)";
+    },false)
+    cards[3].addEventListener("mouseout", function(event){
+        cards[1].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[0].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+        cards[5].style.filter="blur(0px)";
+    },false)
+    cards[4].addEventListener("mouseover", function(event){
+        cards[1].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[0].style.filter="blur(5px)";
+        cards[5].style.filter="blur(5px)";
+    },false)
+    cards[4].addEventListener("mouseout", function(event){
+        cards[1].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[0].style.filter="blur(0px)";
+        cards[5].style.filter="blur(0px)";
+    },false)
+    cards[5].addEventListener("mouseover", function(event){
+        cards[1].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+        cards[0].style.filter="blur(5px)";
+    },false)
+    cards[5].addEventListener("mouseout", function(event){
+        cards[1].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+        cards[0].style.filter="blur(0px)";
+    },false)
+}
+
 
