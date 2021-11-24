c0c797d301053156d1b32cbda57165639e7c910e
KrotovPetr <83280920+KrotovPetr@users.noreply.github.com>
2021-11-24 20:01:24 +0300

    ADD SOME PAGES

---
+++ HumanPages/HumanPage1/dropdown.js
@@ -0,0 +1,77 @@
+
+
+function myFunction4(e){
+    let cards=document.getElementsByClassName("filmscard");
+    cards[0].addEventListener("mouseover", function(event){
+        cards[0].setAttribute("active","true");
+        cards[1].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+    },false);
+    cards[0].addEventListener("mouseout", function(event){
+        cards[0].setAttribute("active","false");
+        cards[1].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+    },false);
+
+    cards[1].addEventListener("mouseover", function(event){
+        cards[0].setAttribute("active","true");
+        cards[0].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+    },false)
+    cards[1].addEventListener("mouseout", function(event){
+        cards[0].setAttribute("active","false");
+        cards[0].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+    },false)
+    cards[2].addEventListener("mouseover", function(event){
+        cards[0].setAttribute("active","true");
+        cards[1].style.filter="blur(5px)";
+        cards[0].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+    },false)
+    cards[2].addEventListener("mouseout", function(event){
+        cards[0].setAttribute("active","false");
+        cards[1].style.filter="blur(0px)";
+        cards[0].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+    },false)
+    cards[3].addEventListener("mouseover", function(event){
+        cards[0].setAttribute("active","true");
+        cards[1].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[0].style.filter="blur(5px)";
+        cards[4].style.filter="blur(5px)";
+    },false)
+    cards[3].addEventListener("mouseout", function(event){
+        cards[0].setAttribute("active","false");
+        cards[1].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[0].style.filter="blur(0px)";
+        cards[4].style.filter="blur(0px)";
+    },false)
+    cards[4].addEventListener("mouseover", function(event){
+        cards[0].setAttribute("active","true");
+        cards[1].style.filter="blur(5px)";
+        cards[2].style.filter="blur(5px)";
+        cards[3].style.filter="blur(5px)";
+        cards[0].style.filter="blur(5px)";
+    },false)
+    cards[4].addEventListener("mouseout", function(event){
+        cards[0].setAttribute("active","false");
+        cards[1].style.filter="blur(0px)";
+        cards[2].style.filter="blur(0px)";
+        cards[3].style.filter="blur(0px)";
+        cards[0].style.filter="blur(0px)";
+    },false)
+
+}
