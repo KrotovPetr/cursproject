85799f76a65bb5e7e2c9399a0b8a8dc430c87c30
KrotovPetr <83280920+KrotovPetr@users.noreply.github.com>
2021-12-18 14:11:40 +0300

    fix some bugs

--- MainlPage/blurfunction.js
+++ MainlPage/blurfunction.js
@@ -3,11 +3,9 @@
 function myFunction4(e){
     let cards=document.getElementsByClassName("newsCard");
     cards[0].addEventListener("mouseover", function(event){
-        cards[1].style.filter="blur(5px)";
-        cards[2].style.filter="blur(5px)";
-        cards[3].style.filter="blur(5px)";
-        cards[4].style.filter="blur(5px)";
-        cards[5].style.filter="blur(5px)";
+      for(let i=1;i<=5;i++){
+          cards[i].style.filter="blur(5px)";
+      }
     },false);
     cards[0].addEventListener("mouseout", function(event){
         cards[1].style.filter="blur(0px)";
